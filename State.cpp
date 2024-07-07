d151f9470f29e8d11a1c33afa12a7eb3b90eef47
VeraSupremo <martiniverinirx@gmail.com>
2024-07-07 00:18:54 -0400

    6
    funcion win creada la val se parecera

--- State.cpp
+++ State.cpp
@@ -75,7 +75,7 @@
 
   // returns an estimate on who is winning. Positive values up to WIN means current player is winning, 
   // negative values down to -WIN means opponent is winning, 
-  // 0 means draw.
+  // 0 means draw.           da una estimacion de quien va ganando o si van empatados
   int State::eval() const
   {
     //IMPLEMENTAR
@@ -90,30 +90,68 @@
             int cuentaMAX= 0,cuentaMIN = 0,cuentaVacia = 0;
             for(int i= 0; i< K;++i){
               int xX = x+i*xX;
-              int yY = y+i*yY;
+              int yY = y+ i*yY;
               if(xX < 0 || xX >= N || yY < 0 || yY >= M) break;
 
-              if(State.sq[yY][xX] == MAX ) cuentaMAX++;
-              else if(State.sq[yY][xX] == MIN ) cuentaMIN++;
+              if(State.sq[yY][xX] == MAX ){
+                cuentaMAX++;  //suma un min cad avez que hay un x
+              } 
+              else if(State.sq[yY][xX] == MIN ){
+                cuentaMIN++; //suma un min cad avez que hay un o
+              } 
               else cuentaVacia++;
               // ahora se evaluaran para entregar puntajes, ver mañana ando morido de sueño 
-
+              if(cuentaMAX == K-1 && cuentaVacia == 1){
+                score += 10;
+              } 
+              if(cuentaMIN == K-1 && cuentaVacia == 1){
+                score -= 10;
+              } 
               
             }
           }
         }
       }
     }
-    return 0;
+    return score * to_move;
   }
 
-  // returns WIN if MAX won, -WIN if MIN won and 0 otherwise.
   int State::win() const
-  {
-    //IMPLEMENTAR
+{
+  //IMPLEMENTAR
+    int score= 0;
+    // score += 5; cout<<"SUMA"<<endl;
+    //ahora toca recorrer los sentidos del tablero(?)       se utilizaran x e y para horizontal y diagonal ^w^
+    for(int y= 0; y <N; ++y){
+      for(int x= 0; x <M; ++x){
+        //ahora diagonal
+        for(int yY = -1; yY<=1; ++yY){
+          for(int xX = -1; xX <= 1; ++xX){
+            if(xX == 0 && yY== 0) continue;// esto evita que se este viendo la misma posicion ^w^
+            int cuentaMAX= 0,cuentaMIN = 0,cuentaVacia = 0;
+            for(int i= 0; i< K;++i){
+              // cout<<"x: "<<x<<" y: "<<y<<" i: "<<i<<" xX: "<<xX<<" yY: "<<yY<<endl;
+              int _xX = x+i*xX;
+              int _yY = y+i*yY;
+              // cout<<"_xX: "<<_xX<<" _yY: "<<_yY<<endl;
+              if(_xX < 0 || _xX >= M || _yY < 0 || _yY >= N) break;
 
+              if(sq[_yY][_xX] == MAX ) cuentaMAX++;
+              else if(sq[_yY][_xX] == MIN ) cuentaMIN++;
+              else cuentaVacia++;
+
+              // cout<<"cuentaMAX: "<<cuentaMAX<<" cuentaMIN: "<<cuentaMIN<<" cuentaVacia: "<<cuentaVacia<<"["<<y<<"]["<<x<<"]"<<endl; 
+              // ahora se evaluaran para entregar puntajes, ver mañana ando morido de sueño
+              if(cuentaMAX == K && cuentaVacia == 0) { return 10;}
+              if(cuentaMIN == K && cuentaVacia == 0) { return -10;}
+            }
+          }
+        }
+      }
+    }
+    // score * to_move
     return 0;
-  }
+}
 
   // make move (x, y) for player to_move
   // and return true iff move is legal
